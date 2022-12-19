fc4b210ec6f5d05038030f06d6b5c8a20dd1218d
Georgii Ozhdikhin <gmozhdikhin@domclick.ru>
2022-12-19 05:50:02 +0300

    small refacoring

--- infrastructure/kafka.py
+++ infrastructure/kafka.py
@@ -3,30 +3,37 @@ from itertools import islice
 from typing import List
 import orjson
 import pandas as pd
+from pandas import DataFrame
 from pykafka import KafkaClient
 from pykafka.common import OffsetType
 from pykafka.exceptions import SocketDisconnectedError, LeaderNotAvailable
-from asyncio import sleep
-from streamlit.delta_generator import DeltaGenerator
 from settings import TIMEFRAME_MAP
 
 
 class KafkaConsumer:
-    def __init__(self, kafka_client: KafkaClient, ticker: str, timeframe: str = 60):
+    def __init__(self, kafka_client: KafkaClient, ticker: str):
+        """
+        Класс отвечает за загрузку данных с Kafka.
+        preload - Загружает исторические данные в с соответствии с timeframe (если имеются)
+        consume_singe_message - загружает одно сообщение (каждый раз следующее)
+        :param kafka_client:
+        :param ticker:
+        :param timeframe:
+        """
         self._kafka_client: KafkaClient = kafka_client
-        self._timeframe: int = TIMEFRAME_MAP.get(timeframe, 60)
-        self._topic = self._kafka_client.topics[ticker]
-        self._last_topic_offset = self._topic.latest_available_offsets()[0].offset[0]
-        self._consumer = self._topic.get_simple_consumer(consumer_timeout_ms=10000,
+        self._ticker = ticker
+        topic = self._kafka_client.topics[ticker]
+        self._last_topic_offset = topic.latest_available_offsets()[0].offset[0]
+        self._consumer = topic.get_simple_consumer(consumer_timeout_ms=10000,
                                                          auto_offset_reset=OffsetType.LATEST,
                                                          reset_offset_on_start=True,
                                                          )
-        self._data: List[List[pd.Timestamp, int]] = []
 
-    async def preload(self) -> None:
+    async def preload(self, timeframe: str = 60) -> DataFrame:
+        timeframe: int = TIMEFRAME_MAP.get(timeframe, 60)
+        data: List[List[pd.Timestamp, int]] = []
         try:
-            print("max_points", self._timeframe)
-            max_partition_rewind = int(math.ceil(self._timeframe / len(self._consumer._partitions)))
+            max_partition_rewind = int(math.ceil(timeframe / len(self._consumer._partitions)))
             offsets = []
             for p, op in self._consumer._partitions.items():
                 if op.last_offset_consumed - max_partition_rewind > -1:
@@ -34,31 +41,21 @@ class KafkaConsumer:
                 else:
                     offsets.append((p, 0))
             self._consumer.reset_offsets(offsets)
-            print("offsets", offsets)
-            for message in islice(self._consumer, min(self._last_topic_offset, self._timeframe)):
+            for message in islice(self._consumer, min(self._last_topic_offset, timeframe)):
                 message = orjson.loads(message.value)
-                self._data.append([pd.to_datetime(message[0]), message[1]])
+                data.append([pd.to_datetime(message[0]), message[1]])
         except (SocketDisconnectedError, LeaderNotAvailable) as e:
             self._consumer.stop()
             self._consumer.start()
-
-    async def consume(self, chart: DeltaGenerator) -> None:
-        try:
-            while True:
-                message = self._consumer.consume()
-                if message:
-                    message = orjson.loads(message.value)
-                    try:
-                        df = pd.DataFrame([[pd.to_datetime(message[0]), message[1]]], columns=["datetime", "value"])
-                        df = df.set_index("datetime")
-                        chart.add_rows(df)
-                    except:
-                        return
-                await sleep(0.5)
-        except:
-            await sleep(1)
-
-    def get_data(self):
-        df = pd.DataFrame(self._data, columns=["datetime", "value"])
+        df = pd.DataFrame(data, columns=["datetime", self._ticker])
         df = df.set_index("datetime")
         return df
+
+    async def consume_singe_message(self):
+        try:
+            message = self._consumer.consume()
+        except (SocketDisconnectedError, LeaderNotAvailable) as e:
+            self._consumer.stop()
+            self._consumer.start()
+            return e
+        return message
